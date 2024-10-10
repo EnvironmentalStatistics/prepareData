d9f09cd5aa7503ae759d26028f6b03895bccf90f
KMicha <michael.kahle@yahoo.de>
2024-10-10 11:16:02 +0200

    add meta data

--- code/insulgas.R
+++ code/insulgas.R
@@ -1,3 +1,4 @@
+library(csvwr)
 library(faraway)
 data(insulgas)
 summary(insulgas)
@@ -6,9 +7,34 @@ write.table(insulgas, file = "data/insulgas/insulgas.csv", append = FALSE, quote
             eol = "\n", na = "NA", dec = ".", row.names = FALSE,
             col.names = TRUE, qmethod = "escape", fileEncoding = "UTF-8")
 
-png(filename="data/insulgas/insulgas.png")
+png(
+ filename="data/insulgas/insulgas.png",
+ width = 3.0,
+ height = 3.0,
+ units = "in",
+ res = 1200,
+ pointsize = 4
+)
+par(
+ mar = c(5,5,2,2),
+ xaxs = "i",
+ yaxs = "i",
+ cex.axis = 2,
+ cex.lab = 2
+)
 plot(Gas ~ Temp, data=insulgas, col=ifelse(insulgas$Insulate=="Before", "blue", "orange"))
 dev.off()
 
 
 
+
+#https://www.stevenfirth.com/csv-on-the-web-creating-descriptive-metadata-files/
+#https://cran.r-project.org/web/packages/csvwr/vignettes/read-write-csvw.html
+#https://csvw.org/
+
+schema <- derive_table_schema(insulgas)
+tb <- list(url="insulgas.csv", tableSchema=schema)
+meta <- create_metadata(tables=list(tb))
+json <- jsonlite::toJSON(meta)
+json2 <- jsonlite::prettify(json)
+cat(json2, file="data/insulgas/insulgas.json")
--- code/insulgas.sh
+++ code/insulgas.sh
@@ -2,6 +2,7 @@
 echo $(pwd)
 
 # First, install packages
+R -e "install.packages('csvwr')"    # move to coding
 R -e "install.packages('faraway')"
 # 
 mkdir -p ./data/insulgas
