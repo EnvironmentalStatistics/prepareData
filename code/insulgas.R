491a55e9cbf073ebf30c5c343bd7174bc4ba2e5e
KMicha <michael.kahle@yahoo.de>
2024-10-10 09:55:47 +0200

    add csv and png

--- code/insulgas.R
+++ code/insulgas.R
@@ -2,4 +2,13 @@ library(faraway)
 data(insulgas)
 summary(insulgas)
 
+write.table(insulgas, file = "data/insulgas/insulgas.csv", append = FALSE, quote = TRUE, sep = ",",
+            eol = "\n", na = "NA", dec = ".", row.names = FALSE,
+            col.names = TRUE, qmethod = "escape", fileEncoding = "UTF-8")
+
+png(filename="data/insulgas/insulgas.png")
 plot(Gas ~ Temp, data=insulgas, col=ifelse(insulgas$Insulate=="Before", "blue", "orange"))
+dev.off()
+
+
+
--- code/insulgas.sh
+++ code/insulgas.sh
@@ -4,6 +4,7 @@ echo $(pwd)
 # First, install packages
 R -e "install.packages('faraway')"
 # 
+mkdir -p ./data/insulgas
 R -f ./code/insulgas.R
 
 
